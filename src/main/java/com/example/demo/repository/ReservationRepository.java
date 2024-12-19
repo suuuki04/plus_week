518d8d4c29d12d1d00e9223e46f14816a7252d75
suki <astre15@naver.com>
2024-12-19 13:55:00 +0900

    feat: N+1 문제 해결
    1.N+1 문제 해결

--- src/main/java/com/example/demo/repository/ReservationRepository.java
+++ src/main/java/com/example/demo/repository/ReservationRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
@@ -18,6 +17,12 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     List<Reservation> findByUserId(Long userId);
 
     List<Reservation> findByItemId(Long itemId);
+
+    @Query("SELECT r FROM Reservation r " +
+    "JOIN FETCH r.user u " +
+    "JOIN FETCH r.item i")
+    List<Reservation> findAll();
+
 
     @Query("SELECT r FROM Reservation r " +
             "WHERE r.item.id = :id " +
